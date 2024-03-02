eac6590149ef87037544891149f1b6b80725977b
João Miranda <50277692+JoaovMiranda@users.noreply.github.com>
2024-03-02 18:18:24 -0300

    :sparkles: Creating user and friend relation

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-  root "articles#index"
+  devise_for :users
+  get "/friends", to: "friends#index", as: "friends_index"
+  resources :friends
+
+  get "/articles", to: "articles#index", as: "articles_index"
   resources :articles do
     resources :comments
   end
-  # get "/articles", to: "articles#index"
-  # get "/articles/:id", to: "articles#show"
-  # get "up" => "rails/health#show", as: :rails_health_check
+
+  get "/home/about", to: "home#about", as: "about" # Rota para a página "Sobre"
+  root "home#index"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_29_142341) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_02_185035) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -27,6 +27,30 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_29_142341) do
     t.datetime "updated_at", null: false
     t.string "status"
     t.index ["article_id"], name: "index_comments_on_article_id"
+  end
+
+  create_table "friends", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "phone"
+    t.string "social_media"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_friends_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "comments", "articles"
